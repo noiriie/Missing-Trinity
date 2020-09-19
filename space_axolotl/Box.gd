21d80bf8a05b17bc0ad8dce93ef908723ff216a5
Seth Dayawansa <s.daya@utmail.utexas.edu>
2020-09-18 23:27:06 -0500

    Pick box

--- space_axolotl/Box.gd
+++ space_axolotl/Box.gd
@@ -4,7 +4,7 @@ const UP = Vector2(0, -1)
 export var gravity = 300
 
 var motion = Vector2();
-
+var picked = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_to_group("bodies")
@@ -13,3 +13,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	motion.y += gravity
+	if picked == true:
+		self.position = get_node("Position2D").global_position
+		
+
+
+func _input(event):
+	if Input.is_action_just_pressed("e"):
+		var bodies = $Detector.get_overlapping_bodies()
+		for b in bodies:
+			print(b)
+			if b.name == "Player" and picked == false:
+				picked = true
