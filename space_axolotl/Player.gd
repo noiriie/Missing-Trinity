e1dd9aea5994463effe12ebb52dc319fb832c883
Sameer Bibikar <bibikar@utexas.edu>
2020-09-18 19:04:01 -0500

    Add death animation/reset scene, make SceneChangeBox actually change scene

--- space_axolotl/Player.gd
+++ space_axolotl/Player.gd
@@ -6,23 +6,44 @@ const ACCELERATION = 50
 export var speed = 400
 export var jump_height = -550
 export var inertia = 100
+export var max_dead_time = 0.6
 
+signal dying
 signal dead
 
 var motion = Vector2();
+var dying = false
+var dying_time = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	dying = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func _start_dying():
+	emit_signal("dying")
+	dying = true
+	dying_time = 0
+
 
 func _physics_process(delta):
+	if dying:
+		# play slow falling animation
+		print(motion.y)
+		motion.x = 0
+		motion.y /= exp(dying_time*2)
+		dying_time += delta
+		if dying_time >= max_dead_time:
+			emit_signal("dead")
+		else:
+			motion = move_and_slide(motion, UP)
+		return
+		
 	motion.y += gravity
 	
 	if Input.is_action_pressed("ui_right"):
@@ -48,10 +69,8 @@ func _physics_process(delta):
 
 func _on_PlayerHitbox_body_entered(body):
 	if body.name == "Traps":
-		emit_signal("dead")
-	
-
+		_start_dying()
 
 func _on_PlayerHitbox_area_entered(area):
 	if area.name == "SceneChangeBox":
-		print(area.target_scene)
+		get_tree().change_scene("res://" + area.target_scene + ".tscn")
