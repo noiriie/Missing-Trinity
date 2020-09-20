a0df0e7d8a94e89f80aa719cfceefef9f35a2b33
Seth Dayawansa <s.daya@utmail.utexas.edu>
2020-09-20 04:41:29 -0500

    end

--- space_axolotl/CreditsSpace.gd
+++ space_axolotl/CreditsSpace.gd
@@ -3,4 +3,4 @@ extends Control
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene("res://CreditsSpace.tscn")
+		get_tree().change_scene("res://Title Screen Main.tscn")
